29652c01fc3d7c532c8b317fbec87dd2eede989f
kinxori <116584982+kinxori@users.noreply.github.com>
2023-04-24 18:35:46 -0600

    renaming things

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -3,10 +3,10 @@ import './table.css';
 export default function Table({children}){
     return (
         <div className="grid">
-            <h3>Nombre</h3>
-            <h3>Apellido</h3>
-            <h3>Correo</h3>
-            <h3>Deuda</h3>
+            <h3>Name</h3>
+            <h3>Lastname</h3>
+            <h3>Email</h3>
+            <h3>Debt</h3>
 
             {children}
         </div>
